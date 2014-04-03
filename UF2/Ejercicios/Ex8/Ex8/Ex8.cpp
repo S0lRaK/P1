393292c8dc2b160431153c185c44563bc269d826
Carlos J. García Carmona <carlosj.garcia.carmona@gmail.com>
2014-04-03 11:23:05 +0200

    Exercici 8 menu

--- UF2/Ejercicios/Ex8/Ex8/Ex8.cpp
+++ UF2/Ejercicios/Ex8/Ex8/Ex8.cpp
@@ -1,25 +1,28 @@
 /*	Heu de realitzar la llibreria (el .h i el .cpp) amb les funcions per tractar un vector d’enters sense importar la seva longitud.
 	Les funcions que s’han d’implementar són:
-		Una funció per introduir dades en el vector. Li passarem el vector i les posicions que volem introduir i retornarà el vector omplert. 
-		Una funció per mostrar les dades del vector per pantalla. Li passarem el vector i la posició fins a la que volem mostrar.
-		Una funció que retorni el vector ordenat de forma ascendent. Li passarem el vector i fins a quina posició volem ordenar.
-		Una funció que retorni la mitja aritmètica dels elements del vector. Li passarem el vector i la posició fins a la qual volem calcular la mitja.
-		Una funció que retorni l’enter mínim del vector. Li passarem el vector i la posició fins a la qual volem calcular el mínim.
-		Una funció que retorni l’enter màxim del vector. Li passarem el vector i la posició fins a la qual volem calcular el màxim.
-		Una funció per copiar un vector en un altre vector. Li passarem el vector que volem copiar i el vector on volem copiar.
-		Una funció per buscar un element en el vector. Li passarem el vector, fins a quina posició volem buscar i l’element que volem buscar.
+		[1]Una funció per introduir dades en el vector. Li passarem el vector i les posicions que volem introduir i retornarà el vector omplert. 
+		[2]Una funció per mostrar les dades del vector per pantalla. Li passarem el vector i la posició fins a la que volem mostrar.
+		[3]Una funció que retorni el vector ordenat de forma ascendent. Li passarem el vector i fins a quina posició volem ordenar.
+		[4]Una funció que retorni la mitja aritmètica dels elements del vector. Li passarem el vector i la posició fins a la qual volem calcular la mitja.
+		[5]Una funció que retorni l’enter mínim del vector. Li passarem el vector i la posició fins a la qual volem calcular el mínim.
+		[6]Una funció que retorni l’enter màxim del vector. Li passarem el vector i la posició fins a la qual volem calcular el màxim.
+		[7]Una funció per copiar un vector en un altre vector. Li passarem el vector que volem copiar i el vector on volem copiar.
+		[8]Una funció per buscar un element en el vector. Li passarem el vector, fins a quina posició volem buscar i l’element que volem buscar.
 			Ens ha de retornar un booleà per dir si l’element existeix en el vector o no i la posició dintre del vector on es troba l’element. 
 	Heu de fer un programa per poder provar totes les funcions programades en la llibreria. 
 */
 
 #include <stdio.h>
-#include <stdlib.h> // Memòria dinàmica
-#include <vector>	// Llibreria personalitzada
+//#include <stdlib.h> // Memòria dinàmica
+#include "Vector.h"	// Llibreria personalitzada
+
+#define MAX 5
 
 void main()
 {
-	int vector[1];
+	int vector[MAX];
+	printf("\n El vector te %d posicions.", MAX);
 
-	//vector = (int *)malloc(sizeof(int));
+	menu(vector);
 	
 }
--- UF2/Ejercicios/Ex8/Ex8/Vector.h
+++ UF2/Ejercicios/Ex8/Ex8/Vector.h
@@ -1,11 +1,42 @@
 #include <stdio.h>
 
+void menu(*vector)
+{
+	int op;
+
+	do
+	{
+		printf("\n [1] Introduir les dades del vector");
+		printf("\n [2] Mostrar el vector");
+		printf("\n [3] Ordenar el vector de forma ascendent");
+		printf("\n [4] Mostrar la mitja aritmetica dels elements del vector");
+		printf("\n [5] Mostrar l'enter minim del vector");
+		printf("\n [6] Mostrar l'enter maxim del vector");
+		printf("\n [7] Copiar el vector en un altre");
+		printf("\n [8] Buscar un element en el vector");
+		printf("\n\n");
+		scanf("%d", &op);
+
+		switch(op)
+		{
+		case 1: introDades(vector);
+			break;
+		case 2: mostrar(vector);
+			break;
+		case 3: 
+		case 4: 
+		case 5: 
+		case 6: 
+		case 7: 
+		case 8: 
+		}
+	}while (op !=9)
+}
+
 int introDades (int *vector)
 {
-	int i, pos;
-	printf("\n De quantes posicions vols el vector?: ");
-	scanf("%d", pos);
-	for(i = 0; i < pos; i++)
+	int i;
+	for(i = 0; i < MAX; i++)
 	{
 		printf("\n Introdueix un valor enter per a la posicio %d: ", i+1);
 		scanf("%d", vector);
@@ -13,13 +44,13 @@ int introDades (int *vector)
 	return *vector;
 }
 
-void mostrar (int *vector, int *pos)
+void mostrar (int *vector, int pos)
 {
 	printf("\n El vector te %d posicions.", pos);
 	printf("\n Fins a quina posicio vols mostrar?: ");
 	scanf("%d", pos);
 	int i;
-	for (i = 0; i < *pos; i++)
+	for (i = 0; i < pos; i++)
 	{
 		printf("%d, ", *vector);
 	}
