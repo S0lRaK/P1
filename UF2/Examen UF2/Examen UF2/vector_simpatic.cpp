0e1217c5dfc7f9e091040a013ef54dc24e3a9022
Carlos J. García Carmona <carlosj.gc989@gmail.com>
2014-06-15 20:43:41 +0200

    Examen UF2 final y revisado

--- UF2/Examen UF2/Examen UF2/vector_simpatic.cpp
+++ UF2/Examen UF2/Examen UF2/vector_simpatic.cpp
@@ -15,29 +15,40 @@ void main()
 	float vector[NUM_POS];
 	float max;
 	
+	// Mostra les posicions a omplir del vector
+	printf("\n----- El vector és de %d posicions -----\n", NUM_POS);
+	
+	// Crida a la funció per a omplir el vector
 	omplirVector(vector, NUM_POS);
+	// Mostra el vector omplert
+	printf("\n El vector conté els següents %d elements: ", NUM_POS);
+	for(int i = 0; i < NUM_POS; i++)
+		// Ajusta la precisió dels elements a 2 decimals, accedint a la variable de tipus vector modificada
+		printf("%.2f | ", vector[i]);
 	
-	if(comprobar2(vector, NUM_POS, MIN_POS))
+	// Comprova si el vector conté el mínim d'elements establerts
+	if(minElements(vector, NUM_POS, MIN_POS))
+	{
 		printf("\n El vector conté almenys %d elements.", MIN_POS);
-	else
-		printf("\n El vector no arribar a contenir el mínim d'elements (%d).", MIN_POS);
+		// Comprova que tots els elements del vector tinguin valors diferents
+		if(diferenciarElements(vector, NUM_POS))
+		{
+			printf("\n Tots els elements del vector són diferents.");
+			// Comproba si l'element amb valor màxim està a la 1ª posició
+			if(max1Element(vector, NUM_POS, &max))
+			{
+				printf("\n El valor màxim del vector és %.2f i SÍ es troba a la 1ª posició.", max);
+				printf("\n EL VECTOR NO MOLA...");
+			}else{
+				printf("\n El valor màxim del vector és %.2f i NO es troba a la primera posició.", max);
+				printf("\n EL VECTOR ÉS SIMPATIC!");
+			}
+		}else{
+			printf("\n EL VECTOR NO MOLA perquè no tots els elements del vector són diferents.");
+		}
+	}else
+		printf("\n EL VECTOR NO MOLA perquè no arriba a contenir el mínim d'elements (%d).", MIN_POS);
 	
-	if(diferenciar(vector, NUM_POS))
-		printf("\n Tots els elements del vector són diferents.");
-	else
-		printf("\n No tots els elements del vector són diferents.");
-
-	if(max1(vector, NUM_POS, &max))
-		printf("\n El valor màxim del vector és %f i SI es troba a la primera posició.", max);
-	else
-		printf("\n El valor màxim del vector és %f i NO es troba a la primera posició.", max);
-	
-	// avalua les funcions correctament si es comparen a FALSE, pero no a TRUE
-	/*
-	if(comprobar2 == false && diferenciar == false && max1 == false)
-		printf("\n EL VECTOR ÉS SIMPATIC!");
-	else
-		printf("\n EL VECTOR NO MOLA...");
-	*/
 	printf("\n\n");
 	system("pause");
+}
--- UF2/Examen UF2/Examen UF2/vector_simpatic.h
+++ UF2/Examen UF2/Examen UF2/vector_simpatic.h
@@ -2,52 +2,59 @@ void omplirVector(float *vector, int pos)
 {
 	for(int i = 0; i < pos; i++)
 	{
-		printf("\n Introdueix el valor per la posició %d del vector: ", i);
+		printf("\n Introdueix el valor per la posició %d del vector: ", i+1);
 		fflush(stdin);
-		scanf("%f", &vector[i]);
+		// en ser un apuntador, accedim com a una variable i guardem el valor a la posició relativa a l'iterador
+		scanf("%f", vector+i);	
+		if(i == pos-1)
+			printf("\n ----- Vector omplert -----\n");
 	}
 }
 
-bool comprobar2(float *vector, int n_pos, int min_pos)
+bool minElements(float *vector, int n_pos, int min_pos)	// per a què li passem l'apuntador a vector??
 {
-	int i;
-	for(i = 0; i < n_pos; i++);
-	if(i > (min_pos-1))
+	if(n_pos >= (min_pos-1))
 		return true;
 	else
 		return false;
 }
 
-bool diferenciar(float *vector, int n_pos)
+// Comprova que tots els elements del vector són diferents
+bool diferenciarElements(float *vector, int n_pos)
 {
-	int i = 0;
-	float aux = vector[i];
-	
-	for(i = 1; i < n_pos; i++)
+	// compara cada element amb tots els altres (FORx2)
+	for(int i = 0; i < n_pos; i++)
 	{
-		if(aux == vector[i])
-			return false;
+		for(int j = 0; j < n_pos; j++)
+		{
+			// salta la comparació del primer element amb sí mateix
+			if(i != j && *(vector+i) == *(vector+j))
+				return false;
+		}
 	}
 	return true;
 }
 
-bool max1(float *vector, int n_pos, float *max)
+// 
+bool max1Element(float *vector, int n_pos, float *max)
 {
-	int i = 0;
-	float aux = vector[i];
-	
-	for(i = 1; i < n_pos; i++)
+	int aux = 0;
+	// estableix el primer element del vector com el més gran d'inici
+	*max = *vector;
+	// comença per la 2ª posició del vector al haber igualat MAX al 1r
+	for(int i = 1; i < n_pos; i++)
 	{
-		if(vector[i] > aux)
+		// si la següent posició amb la que es compara l'anterior es més gran que aquesta...
+		if(*max < *(vector+i))
 		{
-			aux = vector[i];
-			if(i == (n_pos - 1))
-			{
-				*max = aux;
-				return false;
-			}
+			// ...l'estableix com a la màxima del vector
+			*max = *(vector+i);
+			aux = i;
 		}
 	}
-	*max = aux;
-	return true;
+	// si l'auxiliar no ha canviat, vol dir que la 1ª posició del vector és el seu element més gran
+	if(aux == 0)
+		return true;
+	else
+		return false;
 }
